9f80ba7373d31794fa53dccb62c4acffdbd5fbd1
apollon <a.izamylove@gmail.com>
2017-10-31 14:21:36 +0300

    fixed d.py

--- d.py
+++ d.py
@@ -2,18 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import smtplib
-import conf
+import cnf
 import os
 from email.mime.text import MIMEText
 
-msg['Subject'] = 'Disk usage'
-msg['From'] = me
-msg['To'] = you
 s = smtplib.SMTP()
 fs = '/logs'
 st=os.statvfs(fs)
-stat = int((st.f_blocks-st.f_bfree)/float(st.f_blocks)*100)
+stat = str(int((st.f_blocks-st.f_bfree)/float(st.f_blocks)*100))
 msg = MIMEText(stat)
+msg['Subject'] = 'disk usage'
+msg['From'] = me
+msg['To'] = you
+
 
 s.connect()
 s.sendmail(me, you, msg.as_string())
