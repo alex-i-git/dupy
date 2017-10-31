9355311812c632f62b4e1e81c0f2b8f829ed8476
apollon <a.izamylove@gmail.com>
2017-10-31 16:42:22 +0300

    fixed mail addresses import from cnf.py in d.py

--- d.py
+++ d.py
@@ -2,20 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import smtplib
+#импорт почтовых адресов из cnf.py
 import cnf
 import os
 from email.mime.text import MIMEText
 
+#me='apollon@localhost'
+#you='apollon@zenon.net'
 s = smtplib.SMTP()
 fs = '/logs'
 st=os.statvfs(fs)
-stat = str(int((st.f_blocks-st.f_bfree)/float(st.f_blocks)*100))
+stat = str(int((st.f_blocks-st.f_bfree)/float(st.f_blocks)*100)) + '%'
 msg = MIMEText(stat)
-msg['Subject'] = 'disk usage'
-msg['From'] = me
-msg['To'] = you
+msg['Subject'] = 'sadm disk usage'
+msg['From'] = cnf.me
+msg['To'] = cnf.you
 
 
 s.connect()
-s.sendmail(me, you, msg.as_string())
+s.sendmail(cnf.me, cnf.you, msg.as_string())
 s.quit()
